caa3273daaed04868494258a492512357f56a7be
Haru-Kobayashi073 <mndnethnbr.2108@gmail.com>
2022-10-29 14:14:09 +0900

    基本

---
+++ test.py
@@ -0,0 +1,49 @@
+# print("Hello World")
+
+# print(1+1)
+# print(1-1)
+# print(2*2)
+# print(10/2)
+# print(5%3)
+
+# x = 'Hello!'
+# y = 5
+# # print(x)
+# # print(y)
+
+# if x == 'Hello!' :
+#   print('world!')
+
+#関数
+
+# def unko_funbaru(arg):
+#   unko_status = arg
+#   if(unko_status < 10):
+#     return 'mada daijoubu'
+#   else:
+#     return 'yabai'
+
+# # print(unko_funbaru(5))
+
+# for index in range(11):
+#   print(unko_funbaru(index))
+
+# with open('./test.txt','r') as file:
+#   print(file.name)
+
+# class Card:
+#   def __init__(self,date, user_name):
+#     self.date = date
+#     self.user_name = user_name
+#   def message(self):
+#     return 'この投稿は' + self.user_name + 'さんが' + self.date + 'に投稿しました'
+
+# date_a = '2020-01-01'
+# user_name_a = 'Taro'
+
+# card_a = Card(date_a, user_name_a)
+
+# print(card_a.message())
+
+import math
+print(math.pi)
